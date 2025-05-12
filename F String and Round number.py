857f934cbf418f4c142c130ba00a9b4053c42b47
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-13 00:14:14 +0700

    menambah file materi tentang F string dan Pembulatan

---
+++ F String and Round number.py
@@ -0,0 +1,10 @@
+# F string merupakan sebuah formatting dalam fungsi print dimana ia sudah otomatis terconvert jenis data nya ke dalam bentuk string
+# Contoh :
+Total_Buah = 100
+print(f"Buah ini sebanyak {Total_Buah} Buah")
+
+# Pembulatan(Round)
+# Pembulatan dilakukan dalam hasil pembagian atau jenis data float 
+print(10/3)
+print(round(10/3))  # Dalam Pembulatan
+print(round(10/3,2))  # Dalam Pembulatan dan ingin mengambil 2 angka dibelakang koma
