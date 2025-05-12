cd9b327da6ae301f49d12c7bfa4e15a2b950eaf1
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-12 23:43:17 +0700

    Sebuah Contoh Program dengan Komen

--- Komen_Python.py
+++ Komen_Python.py
@@ -1,3 +1,5 @@
 # tanda tagar ini merupakan sebuah komen dimana fungsinya yaitu menandakan sebuah syntax kode agar mudah diingat atau sesuatu yang lain
 # contoh
-
+#Nama merupakan sebuah variabel dimana isinya adalah input dari user
+Nama = input("Masukan Nama : ")
+print(f"Halo {Nama}!")
