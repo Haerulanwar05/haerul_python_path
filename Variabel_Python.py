acfd857c6fa2858a6497597f72ff446fbafae3f8
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-12 23:54:31 +0700

    menambah file baru tentang materi Variabel

---
+++ Variabel_Python.py
@@ -0,0 +1,12 @@
+# Dalam pembuatan variabel di python Harus Masuk akal sehingga mudah diingat 
+# Seperti :
+n = input("Masukan Nama: ") # Ini merupakan contoh penamaan variabel yang salah 
+Nama = input("Masukan Nama : ") # ini merupakan contoh penamaan variabel yang benar
+
+# Di python jika di dalam penamaan variabel terdapat spasi maka tidak boleh kosong harus di isi juga
+# Contoh :
+Angka_Desimal = int(input("Masukan Angka Desimal: "))
+#atau bisa juga digabug dengan menggunakan font CamelCase
+AngkaDesimal = int(input("Masukan Angka Desimal: "))
+
+# Dalam Penamaan variabel juga tidak boleh menggunakan angka atau func yang terdapat di python
