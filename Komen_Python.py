1c65a54a60931b7f1c0d1054401c285f372c3caf
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-12 23:28:43 +0700

    Sebuah File Python tentang Komen

---
+++ Komen_Python.py
@@ -0,0 +1,11 @@
+# ini merupakan sebuah tagar dimana digunakan untuk komen dalam python agar mudah mengingat kode yang telah dibuat
+# contoh :
+
+# Nama merupakan sebuah variabel yang isinya input dari user
+Nama = input("Masukan Nama anda: ")
+print(f"Hallo! {Nama}")
+
+# contoh lain :
+
+# Menghitung Hasil dari 6 * 5
+print(6*5)
