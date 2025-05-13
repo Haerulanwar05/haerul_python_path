426341f76c0837c9510241e13e4cefee5b0cfe06
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-13 12:58:21 +0700

    File Project 1 (Greeting)

---
+++ Project 1 (Greeting).py
@@ -0,0 +1,6 @@
+# Tujuan nya yaitu meminta user menginput nama dan di output terdapan ucapan Greeting
+Nama = input("Masukan Nama Anda: ")
+print(f"Hallo {Nama}")
+print("Welcome to the Python")
+
+
