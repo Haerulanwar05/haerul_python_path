2059e88a1e92c75dce6313c57beba50c5c730659
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-13 13:15:57 +0700

    Project 2 (Group name generator)

---
+++ Project 2.py
@@ -0,0 +1,15 @@
+# membuat Group Name Generator
+'''
+Instruction
+1. Create a greeting for your program.
+2. Ask the user for their favorite color.
+3. Ask the user for their favotire animal.
+4. Combine the name of their favorite color and animal and show them their group name.
+'''
+# Syntax
+Nama = input("Masukan Nama Anda : ")
+print(f"Hallo {Nama}!\nWelcome to Group Name Generator")
+
+Color = input("Masukan Warna Favorit Anda: ")
+Animal = input("Masukan Hewan Kesukaan Anda: ")
+print(f"Group name kamu adalah {Color} {Animal}s")
