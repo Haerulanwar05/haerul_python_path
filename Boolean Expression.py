080065acc5b6131c35e57ad627ba5101a1b33599
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-14 09:57:42 +0700

    File materi Boolean Expression

---
+++ Boolean Expression.py
@@ -0,0 +1,31 @@
+'''
+Boolean merupakan sebuah ekspresi True atau False dalam suatu kondisi
+'''
+# Contoh equal sign '=='
+Hasil = 1==2
+print(Hasil)
+
+# Contoh not equal sign '!='
+Hasil2 = 1 != 2
+print(Hasil2)
+
+# Contoh Greater sign '>'
+Hasil3 = 2 > 1
+print(Hasil3)
+
+# Contoh Less sign '<'
+Hasil4 = 3 < 5
+print(Hasil4)
+
+# Contoh Greater or Equal '>='
+Hasil5 = 4 >= 4
+print(Hasil5)
+
+# Contoh Less or Equal '<='
+print(5<=6)
+
+# Contoh sign ' is not ' tanda ini merupakan sebuah statement dimana tidak bisa digunakan langsung pada angka melainkan terdapat variabel yang mewakilinya
+x = 3
+y = 5
+print(x is not y)
+
