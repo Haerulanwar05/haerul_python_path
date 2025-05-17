1aa8583aa02dcc72f4390f1fdcda7885da94d585
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-05-17 20:25:32 +0700

    Flle Nested Conditional

---
+++ Nested Conditional.py
@@ -0,0 +1,25 @@
+'''
+nested conditionals merupakan sebuah kondisi dimana ketika nilai bernilai True atau False maka dalam kondisi tersebut terdapat kondisi lagi untuk mendapakan nilai atau True lagi
+'''
+# Contoh Syntax 
+'''
+kita membuat salah satu syarat agar bisa masuk karantina dengan kriteria seperti ini:
+Suhu < 39
+umur <= 70
+'''
+# Orang Pertama
+suhu = int(input("Masukan Suhu pengecekan anda: "))
+umur = int(input("Masukan umur anda: "))
+
+if suhu < 39 and umur <= 70:
+    print("Selamatt!,Anda memenuhi Syarat untuk Masuk")
+else:
+    print("Maaf anda tidak memenuhi syarat")
+    Profesi = input("Masukan Profesi Anda: ")
+    if Profesi == "Polisi" or Profesi == "Guru" or Profesi == "Dokter":
+        print("Anda masuk ke pengawasan dalam zona!")
+    else :
+        print("Maaf! kami sudah semaksimal mungkin untuk memperkuat keamanan syarat Karantina kami")
+
+# Ini merupakan salah satu contoh Nested Conditional
+
