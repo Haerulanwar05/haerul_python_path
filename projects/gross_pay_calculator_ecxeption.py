ed7e989a2470f5d9e558971486ab7d679bb9734a
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-06-21 20:36:52 +0700

    perubahan isi file

--- projects/gross_pay_calculator_ecxeption.py
+++ projects/gross_pay_calculator_ecxeption.py
@@ -1,8 +1,32 @@
 '''
 Rewrite Gross Pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program.
 '''
+print("Silahkan Mengecek Gaji kotor anda!")
+
+jam = input("Masukan Total Jam kerja anda? ")
+
 try:
-    quit(jam = (float(input("Masukan Total Jam kerja anda? "))))
-    quit(tarif = (float(input("Masukan tarif per jam anda? "))))
+    jam = float(jam)
+except ValueError:
+    print("anda tidak mengisi sebuah angka harap diulangi!")
+    quit()
+    
+
+tarif = input("Masukan tarif per jam anda? ")
+
+try:
+    tarif = float(tarif)
 except:
     print("anda tidak mengisi sebuah angka harap diulangi!")
+    quit()  # Fungsi quit digunakan untuk keluar dari program jika terjadi error 
+
+
+if jam > 40:
+    upah_lembur = jam-40
+    Hasil_upah_lembur = upah_lembur*tarif*1.5
+    Gaji_Kotor = round(40*tarif+Hasil_upah_lembur,2)
+    print(f"Gaji kotor anda sebesar : ${Gaji_Kotor}")
+else:
+    total_gaji = round(jam*tarif,2)
+    print(f"Gaji kotor anda sebesar : ${total_gaji}")
+
