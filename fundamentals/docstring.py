f9ee33b95b55621cb4a17e502f3c51ecd7f58ad9
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-07-10 08:55:56 +0700

    file fundamental dosctring

---
+++ fundamentals/docstring.py
@@ -0,0 +1,13 @@
+'''
+dostring merupakan sebuah komen dalam python dimana dosctring ini digunakan untuk dokumentasi dalam python khususnya dalam pembuatan fungsi agar mudah diingat fungsi nnya untuk apa, docstring merupakan kalimat ini yang anda baca sekarang.
+'''
+
+# contoh:
+def penjumlahan(a,b):
+    '''
+    ini merupakan sebuah fungsi penjumlahan
+    '''
+    hasil =  a + b
+    return hasil
+
+penjumlahan() # di dalam fungsi tersebut terdapat bacaan dostring yang kita kasih tadi.
