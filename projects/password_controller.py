14358ad4234f6914d25083c55b21e584965558d2
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-07-10 08:50:16 +0700

    update

--- projects/password_controller.py
+++ projects/password_controller.py
@@ -2,6 +2,9 @@
 Buat fungsi yang menggunakan string password sebagai parameter dan memeriksa panjang kata sandi. Jika panjangnya lebih dari 8 karakter, fungsi akan mengembalikan True, jika tidak, akan mengembalikan False.
 '''
 def password_controller(password):
+    ''' ini merupakan sebuah fungsi password untuk mengecek panjang 
+    karakter password
+    '''
     panjang_password = len(password)
     if panjang_password > 8:
         return True
