a3e438d3ee0c084a86104cf9e23f5e39c8976c85
Haerulanwar05 <muh.haerulanwar05@gmail.com>
2025-08-23 13:55:14 +0700

    add escape sequence string examples and explanations to notes

---
+++ fundamentals/escape_sequence_string.py
@@ -0,0 +1,16 @@
+# escape sequence string
+
+my_string = "nama saya adalah M. Haerul Anwar dipanggil \"Haerul\""
+print(my_string)
+'''
+jadi pada dasarnya perintah backslash (\) merupakan sebuah perintah khusus dalam python untuk terlepas dari eksekusi kode agar tidak error atau keliru dalam string 
+'''
+# New Line
+print("Baris 1\nBaris 2")
+
+# tab
+print("\ttab 1\ttab 2")
+
+# range
+print(R"C:Python\belajar\target.") # jadi R semacam memberi tahu hanya fokus string "" dan tidak memperhatikan dalamannya.
+
